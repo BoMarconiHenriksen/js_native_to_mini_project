81892540f8e97ef8cc396db81bd92a129ae59520
Bo Henriksen <bomarconi@gmail.com>
2018-12-06 11:05:47 +0100

    Oprydning i editUserScreen.

--- mini_project/screens/EditUserScreen.js
+++ mini_project/screens/EditUserScreen.js
@@ -1,45 +1,6 @@
 import React from 'react';
 import { Text, View, StyleSheet, ScrollView, TextInput, Button } from 'react-native';
-import gql from "graphql-tag";
 import GetUserByUserName from '../apollo/GetUserByUserName';
-
-let getUserById = gql`
-query User($id: ID!) { 
-    getUserById(id: $id) {
-        id
-        userName
-        firstName
-        lastName
-        password
-        email
-  }
-}
-`; 
-
-const GetUserById = ({id}) => (
-    <Query query={getUserById} variables={{ id }} >
-      
-      {({ loading, error, data }) => {
-        if (loading) return <Text>Loading...</Text>;
-        if (error) return `Error! ${error.message}`;
-  
-        return data.getUserByName.map(({ id, userName, firstName, lastName, password, email }) => (
-          <View key={id}>
-            <Text>{`
-                    ID: ${id} 
-                    Username: ${userName} 
-                    Firstname: ${firstName} 
-                    Lastname: ${lastName} 
-                    Password: ${password} 
-                    Email: ${email}`
-                  }</Text>
-          </View>
-          
-        ));
-        
-      }}
-    </Query>
-  );
 
 export default class EditUserScreen extends React.Component {
     constructor(props) {
