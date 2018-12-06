02de0be9c815ae1547398ce436722a0ae2ef2c99
Bo Henriksen <bomarconi@gmail.com>
2018-12-06 15:28:21 +0100

    Arbejdet lidt mere med createUser. Virker ikke.

--- mini_project/apollo/CreateUser.js
+++ mini_project/apollo/CreateUser.js
@@ -1,27 +1,41 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import gql from "graphql-tag";
 import { Text, View } from 'react-native';
+import users from '../apollo/Users';
+// https://codesandbox.io/s/v3mn68xxvy
+// https://www.apollographql.com/docs/react/essentials/mutations.html#basic
 
 let createUserQuery = gql`
-mutation User($userName: String!, $firstName: String!, $lastName: String!, $password: String!, $email: String!) { 
+mutation CreateUser($userName: String!, $firstName: String!, $lastName: String!, $password: String!, $email: String!) { 
   createUser(input:{userName: $userName, firstName: $firstName, lastName: $lastName, password: $password, email: $email}) {
-  id
-  userName
-  firstName
-  lastName
-  password
-  email
+    id
+    userName
+    firstName
+    lastName
+    password
+    email
   }
 }
 `; 
 
 const CreateUser = ({ userName, firstName, lastName, password, email }) => (
-  <Mutation mutation={createUserQuery} variables={{ userName, userName, firstName, lastName, password, email }} >
+  <Mutation 
+    mutation={createUserQuery} 
+    variables={{ userName, firstName, lastName, password, email }}
+    update={(cache, { data: { userName, firstName, lastName, password, email } }) => {
+        const { allUsers } = cache.readQuery({ query: users.getAllusersQuery });
+        cache.writeQuery({
+            query: users.getAllusersQuery,
+            data: { allUsers: allUsers.concat([userName, firstName, lastName, password, email]) }
+        })
+    }}
+  >
     
     {({ loading, error, data }) => {
       if (loading) return <Text>Loading...</Text>;
       if (error) return `Error! ${error.message}`;
+    
 
         let view = <View>
           <Text>{`
